bd888fdca4c30943f7fc42b62d34d0bd30b6e490
czyt <czyt@w.cn>
2025-05-15 09:51:34 +0800

    添加字典及样式文件

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,9 +2,8 @@ use std::env;
 use std::path::PathBuf;
 
 pub const MDX_FILES: &[&str] = &[
-    "./resources/mdx/en/牛津高阶8.mdx",
-    "./resources/mdx/en/朗文当代4.mdx",
-    // "./resources/mdx/en/简明必应.mdx",
+    "./resources/mdx/en/牛津高阶英汉双解词典（第9版）.mdx",
+     "./resources/mdx/en/韦氏高阶英汉双解词典v3.mdx",
 ];
 
 pub fn static_path() -> anyhow::Result<PathBuf> {
